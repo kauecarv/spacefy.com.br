a8f36bf4d7f06359c87bd31eb5c71ed6b80dfed6
Maciel <112631220+macielrib@users.noreply.github.com>
2024-10-19 23:56:30 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import NavbarComponent from "./components/navbar/NavbarComponent";
 import WhatsAppButton from "./components/Whatsapp";
