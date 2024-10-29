bb140e3879f498ac110ea0a7e48582e1e790b2f4
Maciel <112631220+macielrib@users.noreply.github.com>
2024-10-28 22:31:29 -0300

    Finalizado landing page

--- src/app/(root)/sections/contact-us/page.tsx
+++ src/app/(root)/sections/contact-us/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 import { MdOutlineCallMissedOutgoing } from "react-icons/md";
 import { motion, useInView } from "framer-motion";
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 import { FaDiscord, FaInstagram, FaLinkedin, FaThreads } from "react-icons/fa6";
 
